57db9bd1c319415eb069a421d738654ce0cb962b
andreyresende <38325819+andreyresende@users.noreply.github.com>
2022-12-11 12:55:44 -0300

    Trabalho em desenvolvimento
    Grafo pronto, fazendo coeficiente de aglomeração

---
+++ Projeto1.py
@@ -0,0 +1,55 @@
+class vertice:
+    def __init__(self, pais):
+        self.lista = []
+        self.pais = pais
+
+    def print_lista(self):
+        print(self.pais, ":", self.lista)
+
+    def adiciona_adjacencia(self, valor):
+        self.lista.append(valor)    
+
+
+def prepara_grafo():
+    f = open("cliques_copas.txt", "r") # Apos abrir o codigo pula 4 linhas de comentario e espaco em branco
+    f.readline()
+    f.readline()
+    f.readline()
+    f.readline()
+    Grafo = []
+    for linha in f:
+        nome = linha.split(":") # Separa o nome do pais do restante da lista
+        node = vertice(nome[0].strip()) # Retira os espacos em branco antes e depois do nome do pais
+        for vizinho in nome[1].split(","): # Para cada vizinho na lista do pais
+            node.adiciona_adjacencia(vizinho.strip(",; \n")) # Adiciona esse vizinho na lista, limpando os caracteres
+        Grafo.append(node) # Adiciona o vertice ao grafo
+    f.close()
+
+    return Grafo
+
+def Bron_kerbosch(Grafo):
+    conjuntoR = []
+    conjuntoP = []
+    conjuntoX = []
+
+def calcula_coeficiente(Grafo):
+    coeficiente_aglomeracao = 0
+    for node in Grafo: # Brasil (lista)
+        possiveis_triangulos = (len(node.lista)*(len(node.lista) - 1))/2
+        triangulos_reais = 0
+        for vizinho in node.lista: # Italia (string)
+            for busca in Grafo: # Busca no grafo a lista da Italia
+                if(busca.pais == vizinho): # Quando acha
+                    if (possivel != node) and (possivel in node.lista):
+                        triangulos_reais += 1
+        coeficiente_aglomeracao += triangulos_reais/possiveis_triangulos
+    return coeficiente_aglomeracao/len(Grafo)
+
+
+if __name__ == '__main__':
+    Grafo = prepara_grafo()
+    for node in Grafo:
+        node.print_lista()
+    #print(calcula_coeficiente(Grafo))
+    print(Grafo[0].lista)
+
