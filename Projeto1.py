7af82366d9946c67d8c9d8b210933f0e20d9f9bc
andreyresende <38325819+andreyresende@users.noreply.github.com>
2022-12-11 16:51:30 -0300

    Comentários melhorados e clique máximo apresentado

--- Projeto1.py
+++ Projeto1.py
@@ -9,6 +9,11 @@ class vertice:
     def adiciona_adjacencia(self, valor):
         self.lista.append(valor)    
 
+class Clique:
+    def __init__(self, tamanho, lista):
+        self.tamanho = tamanho
+        self.lista = lista
+
 
 def prepara_grafo():
     f = open("cliques_copas.txt", "r") # Apos abrir o codigo pula 4 linhas de comentario e espaco em branco
@@ -28,24 +33,29 @@ def prepara_grafo():
     return Grafo
 
 def Bron_kerbosch(conjuntoR, conjuntoP, conjuntoX):# o P recebe todos os objetos vertice, cada um com um nome e uma lista de strings
+    global tamanho_clique_maximo
+    global cliques_maximais
     if not conjuntoP and not conjuntoX:
         nomes = []
-        for membros in conjuntoR:# Se printar o R vai sair um monte de objetos, entao preciso buscar os nomes
+        for membros in conjuntoR:# Se printar o conjuntoR vai sair um monte de objetos, entao preciso buscar os nomes
             nomes.append(membros.pais)
         if len(nomes) >= 3:
-            print("Clique: ", nomes)
-    for node in conjuntoP:# Cada no em P eh um objeto da classe vertice
-###################################################### Tratando R        
+            print("Clique de tamanho ", len(nomes), " encontrado: ", nomes)
+            clique = Clique(len(nomes), nomes)
+            cliques_maximais.append(clique)
+        if len(nomes) > tamanho_clique_maximo:
+            tamanho_clique_maximo = len(nomes)
+    for node in conjuntoP:# Cada no em conjuntoP eh um objeto da classe vertice
+        #################################################### Tratando R        
         copiaR = conjuntoR.copy()
         copiaR.append(node)# Preciso fazer o append antes de enviar como parametro porque o metodo append nao retorna nada, entao se passar R.append() ele chega como None.
-        
-###################################################### Tratando P
+
+        #################################################### Tratando P
         copiaP = [] # Cada no em P eh um objeto da classe vertice, mas a interseccao com a lista de 'node' deve ser tratada porque sao strings
-        #Preciso colocar em copiaP todos os elementos em P cujo elemento.pais esta na lista de 'node'
-        for elemento in conjuntoP: # iterando vertices (objetos)
+        for elemento in conjuntoP:
             if elemento.pais in node.lista:
                 copiaP.append(elemento)
-######################################################
+        #################################################### Tratando X
         copiaX = []
         for elemento in conjuntoX:# Mesmo tratamento do conjunto P fazendo interseccao com a lista de node
             if elemento.pais in node.lista:
@@ -57,10 +67,10 @@ def Bron_kerbosch(conjuntoR, conjuntoP, conjuntoX):# o P recebe todos os objetos
 
 def calcula_coeficiente(Grafo):
     coeficiente_aglomeracao = 0
-    for node in Grafo: # Brasil (lista)
+    for node in Grafo: # Comeca por Brasil (tipo List)
         possiveis_triangulos = (len(node.lista)*(len(node.lista) - 1))/2
         triangulos_reais = 0
-        for vizinho in node.lista: # Italia (string)
+        for vizinho in node.lista: # Primeiro vizinho sera Italia (string)
             for busca in Grafo: # Busca no grafo a lista da Italia
                 if(busca.pais == vizinho): # Quando acha
                     for sub_vizinho in busca.lista: # Busca nessa lista
@@ -70,11 +80,22 @@ def calcula_coeficiente(Grafo):
             coeficiente_aglomeracao += triangulos_reais/possiveis_triangulos
     return coeficiente_aglomeracao/len(Grafo)
 
-
+tamanho_clique_maximo = 0 # Usado como variavel global para encontrar o maximo
+cliques_maximais = []     # Usado como lista global para armazenar os cliques
 if __name__ == '__main__':
     Grafo = prepara_grafo()
+    print("***Grafo:***")
     for node in Grafo:
         node.print_lista()
-    print("Coeficiente de aglomeracao: " + str(calcula_coeficiente(Grafo)))
+    print()
+    print("*******Coeficiente medio de aglomeracao do grafo: " + str(calcula_coeficiente(Grafo)))
+    print()
+    print("Executando Bron-Kerbosch:")
     Bron_kerbosch([],Grafo,[])
-
+    print("O maior clique encontrado possui tamanho ", tamanho_clique_maximo)
+    cliques_maximos = []
+    for clique in cliques_maximais: # Coloca os cliques maximos na lista correta
+        if clique.tamanho == tamanho_clique_maximo:
+            cliques_maximos.append(clique)
+    for clique in cliques_maximos: # Apresenta os cliques maximos
+        print("Clique máximo: ", clique.lista)
